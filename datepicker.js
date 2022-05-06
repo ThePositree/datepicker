0b44d1cdbaf98444229af63a5e69933fc02aa4e2
ThePositree <ThePositree@yandex.ru>
2022-05-06 22:29:40 +0300

    фв

--- datepicker.js
+++ datepicker.js
@@ -1,12 +1,40 @@
 class Datepicker {
   constructor($el) {
 
-    
+
     function f_addZero(p_num) {
       if (p_num >= 0 && p_num <= 9) {
         return '0' + p_num;
       } else {
         return p_num;
+      }
+    }
+
+    function getNameMounth(m) {
+      if (m === 1) {
+        return 'Январь'
+      } else if (m === 2) {
+        return 'Февраль'
+      } else if (m === 3) {
+        return 'Март'
+      } else if (m === 4) {
+        return 'Апрель'
+      } else if (m === 5) {
+        return 'Май'
+      } else if (m === 6) {
+        return 'Июнь'
+      } else if (m === 7) {
+        return 'Июль'
+      } else if (m === 8) {
+        return 'Август'
+      } else if (m === 9) {
+        return 'Сентябрь'
+      } else if (m === 10) {
+        return 'Октябрь'
+      } else if (m === 11) {
+        return 'Ноябрь'
+      } else if (m === 12) {
+        return 'Декабрь'
       }
     }
 
@@ -22,7 +50,7 @@ class Datepicker {
       'Сентябрь',
       'Октябрь',
       'Ноябрь',
-      'Декабрь',
+      'Декабрь'
     ]
     let allDays = [
       'Вс',
@@ -36,24 +64,23 @@ class Datepicker {
     let mounth = allM[new Date().getMonth()]
     let year = new Date().getFullYear()
     let day = new Date().getDate()
-    $el.value = `${year}-${f_addZero(getIndexMounth(mounth))}-${f_addZero(day)}`
+    $el.value = `${year}-${f_addZero(getIndexMounth(mounth) + 1)}-${f_addZero(day)}`
     function getIndexMounth(m) {
-      return allM.indexOf(m) + 1
+      return allM.indexOf(m)
     }
     function getFirstDayOfWeek(m, y) {
-      return new Date(`${y}-${getIndexMounth(m)}-1`).getDay();
+      return new Date(`${y}-${getIndexMounth(m) + 1}-1`).getDay();
     }
     function getLastDayofMounth(m, y) {
-      return new Date(y, getIndexMounth(m), 0).getDate()
+      return new Date(y, getIndexMounth(m) + 1, 0).getDate()
     }
-    function drowBtn(m = allM[new Date().getMonth()], y = new Date().getFullYear(),d = new Date().getDate()) {
+    function drowBtn(m = allM[new Date().getMonth()], y = new Date().getFullYear(), d = new Date().getDate()) {
       let result = '';
-      let amountDaysOfLastMounth = getLastDayofMounth(allM[getIndexMounth(m) - 2], y)
+      let amountDaysOfLastMounth = getLastDayofMounth(allM[getIndexMounth(m) - 1], y)
       let amountDaysOfcurrentMounth = getLastDayofMounth(m, y)
       let amountForDrowAllRow;
       let counter = 0;
       let counter2 = 0;
-      console.log(amountDaysOfLastMounth);
       if (getFirstDayOfWeek(m, y)) {
         amountDaysOfLastMounth = amountDaysOfLastMounth - (getFirstDayOfWeek(m, y) - 2)
         for (let index = 0; index < getFirstDayOfWeek(m, y) - 1; index++) {
@@ -101,7 +128,7 @@ class Datepicker {
       }
       let amountNextMountRestRow = 42 - counter2;
       for (let index = 0; index < amountNextMountRestRow; index++) {
-        result += `<button class="dp-btn-next-mounth">${index+1}</button>`
+        result += `<button class="dp-btn-next-mounth">${index + 1}</button>`
       }
       return result
     }
@@ -140,7 +167,6 @@ class Datepicker {
     $el.insertAdjacentHTML('afterend', this.containerDp)
     this.parentEl = $el.parentNode
     let myDp = this.parentEl.querySelector('.dp-container')
-
     function clickNextYear() {
       document.querySelectorAll('.dp-main button').forEach(element => {
         element.removeEventListener('click', clickСhoose)
@@ -152,12 +178,34 @@ class Datepicker {
         element.addEventListener('click', clickСhoose)
       });
     }
+    function clickNextMounth() {
+      document.querySelectorAll('.dp-main button').forEach(element => {
+        element.removeEventListener('click', clickСhoose)
+      });
+      mounth = allM[getIndexMounth(mounth) + 1]
+      document.querySelector('.dp-nav-m').innerText = mounth
+      document.querySelector('.dp-main').innerHTML = `${drowBtn(mounth, year, day)}`
+      document.querySelectorAll('.dp-main button').forEach(element => {
+        element.addEventListener('click', clickСhoose)
+      });
+    }
     function clickPrevYear() {
       document.querySelectorAll('.dp-main button').forEach(element => {
         element.removeEventListener('click', clickСhoose)
       });
       year = year - 1
       document.querySelector('.dp-nav-y').innerText = year
+      document.querySelector('.dp-main').innerHTML = `${drowBtn(mounth, year, day)}`
+      document.querySelectorAll('.dp-main button').forEach(element => {
+        element.addEventListener('click', clickСhoose)
+      });
+    }
+    function clickPrevMounth() {
+      document.querySelectorAll('.dp-main button').forEach(element => {
+        element.removeEventListener('click', clickСhoose)
+      });
+      mounth = allM[getIndexMounth(mounth) - 1]
+      document.querySelector('.dp-nav-m').innerText = mounth
       document.querySelector('.dp-main').innerHTML = `${drowBtn(mounth, year, day)}`
       document.querySelectorAll('.dp-main button').forEach(element => {
         element.addEventListener('click', clickСhoose)
@@ -181,13 +229,15 @@ class Datepicker {
 
     $el.addEventListener('click', function () {
       myDp.classList.add('dp-container--active')
-    })    
+    })
     document.querySelectorAll('.dp-main button').forEach(element => {
       element.addEventListener('click', clickСhoose)
     });
     document.querySelector('.dp-prev-y').addEventListener('click', clickPrevYear)
+    document.querySelector('.dp-prev-m').addEventListener('click', clickPrevMounth)
     document.querySelector('.dp-next-y').addEventListener('click', clickNextYear)
-    
+    document.querySelector('.dp-next-m').addEventListener('click', clickNextMounth)
+
     document.addEventListener('click', function (e) {
       if (myDp.classList.contains('dp-container--active')) {
         if (!e.target.closest(`.${$el.parentNode.classList[0]}`)) {
